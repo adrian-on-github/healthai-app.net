35bd16d294786cf2844087a3b267fd2a04f14926
Adrian <adrian.hassan.ef@gmail.com>
2025-03-03 21:39:00 +0100

    AnimatedList

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -2,11 +2,15 @@ import HeroSection from "@/components/ui/HeroSection";
 import Navbar from "@/components/ui/Navbar";
 import Features from "@/components/ui/Features";
 import FeaturedOn from "@/components/ui/FeaturedOn";
+import { AnimatedListDemo } from "@/components/ui/AnimatedList";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <div className="absolute top-1 right-70">
+        <AnimatedListDemo />
+      </div>
       <section className="pt-[6vh] px-20 pb-20">
         <HeroSection />
       </section>
