60554fb1da04e6e1c3b95da748a584359ffb23a0
Adrian <adrian.hassan.ef@gmail.com>
2025-03-05 13:46:36 +0100

    real avatars

--- components/ui/HeroSection.js
+++ components/ui/HeroSection.js
@@ -5,48 +5,32 @@ import PromotionButtons from "./PromotionButtons";
 const HeroSection = () => {
   const avatars = [
     {
-      imageUrl: "https://avatars.githubusercontent.com/u/16860528",
+      imageUrl: "https://avatars.githubusercontent.com/u/175122937?v=4",
       profileUrl: "https://github.com/dillionverma",
     },
     {
-      imageUrl: "https://avatars.githubusercontent.com/u/20110627",
+      imageUrl: "https://avatars.githubusercontent.com/u/168353082?v=4",
       profileUrl: "https://github.com/tomonarifeehan",
     },
     {
-      imageUrl: "https://avatars.githubusercontent.com/u/106103625",
+      imageUrl: "https://avatars.githubusercontent.com/u/136450574?v=4",
       profileUrl: "https://github.com/BankkRoll",
     },
     {
-      imageUrl: "https://avatars.githubusercontent.com/u/59228569",
+      imageUrl: "https://avatars.githubusercontent.com/u/86548888?v=4",
       profileUrl: "https://github.com/safethecode",
     },
     {
-      imageUrl: "https://avatars.githubusercontent.com/u/59442788",
+      imageUrl: "https://avatars.githubusercontent.com/u/24898559?v=4",
       profileUrl: "https://github.com/sanjay-mali",
     },
     {
-      imageUrl: "https://avatars.githubusercontent.com/u/89768406",
+      imageUrl: "https://avatars.githubusercontent.com/u/85670037?v=4",
       profileUrl: "https://github.com/itsarghyadas",
     },
     {
-      imageUrl: "https://avatars.githubusercontent.com/u/20110627",
+      imageUrl: "https://avatars.githubusercontent.com/u/73161735?v=4",
       profileUrl: "https://github.com/tomonarifeehan",
-    },
-    {
-      imageUrl: "https://avatars.githubusercontent.com/u/106103625",
-      profileUrl: "https://github.com/BankkRoll",
-    },
-    {
-      imageUrl: "https://avatars.githubusercontent.com/u/59228569",
-      profileUrl: "https://github.com/safethecode",
-    },
-    {
-      imageUrl: "https://avatars.githubusercontent.com/u/59442788",
-      profileUrl: "https://github.com/sanjay-mali",
-    },
-    {
-      imageUrl: "https://avatars.githubusercontent.com/u/89768406",
-      profileUrl: "https://github.com/itsarghyadas",
     },
   ];
   return (
